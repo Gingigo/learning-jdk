682f018438e5fdd60a490e1bb1c689691e9cd723
gin <fandunheng@gmail.com>
2020-06-23 12:20:30 +0800

    init

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author gin
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        List<String> list = new ArrayList<>();
+
+        list.add("s");
+    }
+
+
+}
